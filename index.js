2ec3a48e645f7aed0290bc2426b5dcc6cdffe466
Lukas Mills <lukascodes64@gmail.com>
2022-07-03 18:01:08 -0400

    commit tree 8f249377f8765e7f5f5b2012cef329e8ba4e690c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue){
+    return Math.abs(42-someValue)
+
+
+}
+function distanceFromHqInFeet(someValue) {
+    return Math.abs(42-someValue) * 264
+}
+
+let distanceTravelledInFeet  = (start, destination) => {
+    return Math.abs (start-destination) * 264
+
+}
+
+function calculatesFarePrice (start, destination) {
+    let distanceTravelled = Math.abs(start-destination) * 264
+    
+if (distanceTravelled <= 400) {
+    return  0
+}
+else if (distanceTravelled >= 400 && distanceTravelled <= 2000) {
+    return (distanceTravelled - 400) * 0.02
+}
+else if (distanceTravelled >= 2000 && distanceTravelled <= 2500){
+    return 25
+}
+else if (distanceTravelled > 2500) {
+    return `cannot travel that far`
+}
+     
+}
